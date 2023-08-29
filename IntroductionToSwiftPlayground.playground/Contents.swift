65f2f8deffe2fe809ca26978e3f7ba737b6b4a90
cazlito <142947671+cazlito@users.noreply.github.com>
2023-08-29 13:35:37 -1000

    Added new variables and created a print function
    Still need to complete on my own

--- IntroductionToSwiftPlayground.playground/Contents.swift
+++ IntroductionToSwiftPlayground.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+//strings is a series of characteres in ""
+
+/*use this to comment out in blocks*/
+//this is used for one line only
+
+//var is used for declaring a variable only
+//var myFirstVariable = "I've created my first variable"
+
+var myFirstVariable: String
+
+myFirstVariable = "Sup"
+
+var mySecondVariable = " and look I added 2 strings together"
+
+//part 7 string concantenation??? i dunno how to spell it
+//essentially myFirst + mySecond
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+
+print(myThirdVariable)
+
+
